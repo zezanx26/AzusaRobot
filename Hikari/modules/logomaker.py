78c387f19bbf3d5052dba168ca94c311ab2911cc
Grey Project <95908917+grey423@users.noreply.github.com>
2022-04-03 17:59:19 +0700

    Update logomaker.py

--- Hikari/modules/logomaker.py
+++ Hikari/modules/logomaker.py
@@ -113,7 +113,7 @@ async def logo_gen(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Hikari Robot](https://t.me/HikariBot)",
+            caption="Logo by [Hikari Robot](https://t.me/HikariManageRobot)",
             force_document=False,
         )
         os.remove(flnme)
@@ -196,7 +196,7 @@ async def logo_(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Hikari Robot](https://t.me/HikariBot)",
+            caption="Logo by [Hikari Robot](https://t.me/@HikariManageRobot)",
             force_document=False,
         )
         os.remove(flnme)
