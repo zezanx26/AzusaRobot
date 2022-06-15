ec5cb2ca2bd77775854160e0e755df3f5ef7d75f
zezanx26 <102902672+zezanx26@users.noreply.github.com>
2022-06-15 16:11:51 +0700

    Update alive.py

--- Hikari/modules/alive.py
+++ Hikari/modules/alive.py
@@ -9,16 +9,16 @@ from Hikari.events import register
 from Hikari import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/1be0ff236e8c3de2772c4.jpg"
+PHOTO = "https://telegra.ph/file/884e6797a14d7430ef5b7.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  PRIME = f"**ʜɪ [{event.sender.first_name}](tg://user?id={event.sender.id}), ᴀᴋᴜ ʜɪᴋᴀʀɪ ʀᴏʙᴏᴛ.** \n\n"
+  PRIME = f"**ʜɪ [{event.sender.first_name}](tg://user?id={event.sender.id}), ᴀᴋᴜ ᴀᴢᴜsᴀ ʀᴏʙᴏᴛ.** \n\n"
   PRIME += "🌼 **ᴀᴋᴜ sᴇʟᴀʟᴜ ʜɪᴅᴜᴘ ᴅᴀɴ ʙᴇᴋᴇʀᴊᴀ** \n\n"
-  PRIME += f"🌼 **ᴍʏ ᴅᴀʀʟɪɴɢ : [ʀᴇxᴧ-ᴇx](https://t.me/JustRex)** \n\n"
+  PRIME += f"🌼 **ᴍʏ ᴅᴀʀʟɪɴɢ : [ᴢᴇᴢᴀɴ-ᴇx](https://t.me/wndrslna)** \n\n"
   PRIME += f"🌼 **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
   PRIME += f"🌼 **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
   PRIME += f"🌼 **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n\n"
   PRIME += "**ᴛᴇʀɪᴍᴀᴋᴀsɪʜ sᴜᴅᴀʜ ᴍᴇɴᴀᴍʙᴀʜᴋᴀɴ ᴋᴜ ᴅɪsɪɴɪ ❤️**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/HikariManageRobot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/rexaprivateroom")]]
+  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/AzusaxRobot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/zxzansupport")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=PRIME,  buttons=BUTTON)
